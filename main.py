fb0b6a5f2aea7d9ad02f7e24d2f8630be2a05b9f
livio.gonzalez-luna <livio.gonzalez-luna@laplateforme.io>
2023-01-24 07:59:24 +0100

    Base structure

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
-import tkinter as tk
+from tkinter import *
 import re
 
-def check_password_strength(password):
-    error_message = ""
-    if len(password) < 8:
-        error_message += "• Must contain at least 8 characters.\n"
-    if not re.search("[a-z]", password):
-        error_message += "• Must contain at least one lowercase letter.\n"
-    if not re.search("[A-Z]", password):
-        error_message += "• Must contain at least one uppercase letter.\n"
-    if not re.search("[0-9]", password):
-        error_message += "• Must contain at least one number.\n"
-    if not re.search("[!@#$%^&*]", password):
-        error_message += "• Must contain at least one special character (!, @, #, $, %, ^, &, *).\n"
-    return error_message
 
-def check_password():
-    password = password_entry.get()
-    error_message = check_password_strength(password)
-    if not error_message:
-        message_label.config(text="Password accepted. Welcome, {}.".format(name_entry.get()))
-    else:
-        message_label.config(text="Your password does not meet the following security requirements:\n{}".format(error_message))
 
-root = tk.Tk()
+root = Tk()
 root.title("Password Checker")
+root.geometry("400x400")
 
-name_label = tk.Label(root, text="Name:")
-name_label.grid(row=0, column=0)
+# Check if password is strong enough
+def check_password_strength(password):
+    if len(password) < 8:
+        return False
+    if not re.search("[a-z]", password):
+        return False
+    if not re.search("[A-Z]", password):
+        return False
+    if not re.search("[0-9]", password):
+        return False
+    if not re.search("[!@#$%^&*]", password):
+        return False
+    return True
 
-name_entry = tk.Entry(root)
-name_entry.grid(row=0, column=1)
+# Check if password is correct
+def check_password():
+    name = name_entry.get()
+    password = password_entry.get()
+    if check_password_strength(password):
+        message = "Welcome " + name + "!"
+        label.config(text=message, fg="green")
+    else:
+        message = "Invalid password. Please try again."
+        label.config(text=message, fg="red")
+        password_entry.delete(0, END)
 
-password_label = tk.Label(root, text="Password:")
-password_label.grid(row=1, column=0)
 
-password_entry = tk.Entry(root, show="*")
-password_entry.grid(row=1, column=1)
+name_label = Label(root, text="Name:")
+name_label.grid(row=0, column=0, pady=10)
 
-submit_button = tk.Button(root, text="Submit", command=check_password)
-submit_button.grid(row=2, column=0, columnspan=2, pady=10)
+name_entry = Entry(root)
+name_entry.grid(row=0, column=1, pady=10)
 
-message_label = tk.Label(root)
-message_label.grid(row=3, column=0, columnspan=2)
+password_label = Label(root, text="Password:")
+password_label.grid(row=1, column=0, pady=10)
+
+password_entry = Entry(root, show="*")
+password_entry.grid(row=1, column=1, pady=10)
+
+submit_button = Button(root, text="Submit", command=check_password)
+submit_button.grid(row=2, column=1, pady=10)
+
+label = Label(root)
+label.grid(row=3, column=1, pady=10)
 
 root.mainloop()
